a58af451cb11e2efce14c0d0c4eb44b9685c0e93
Nhan Thai <72413541+NhanThai0501@users.noreply.github.com>
2023-10-05 22:50:20 -0700

    Update SimulationDriver.java

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -10,7 +10,7 @@ public class SimulationDriver {
         
         Random random = new Random();
         // Randomly generate number of student in bound of 50
-        int numStudents = random.nextInt(20, 72);
+        int numStudents = random.nextInt(20, 95);
         
         List<Student> students = generateStudents(numStudents);
         System.out.println("The number of students have been generated: " + numStudents);
@@ -22,7 +22,7 @@ public class SimulationDriver {
 
         // Configure question and generate answers
         System.out.println("\nQuestion 1:");
-        Question question1 = new Question("Multiple-choice", Arrays.asList("A", "B", "C", "D"), true);
+        Question question1 = new Question("Multiple choices", Arrays.asList("A", "B", "C", "D"), true);
         VotingService votingService1 = new VotingService();
         System.out.println(question1.getType());
         votingService1.configureQuestion(question1);
@@ -31,7 +31,7 @@ public class SimulationDriver {
 
         
         System.out.println("Question 2:");
-        Question question2 = new Question("Single-choice", Arrays.asList("1. True", "2. False"), false);
+        Question question2 = new Question("Single choice", Arrays.asList("1. True", "2. False"), false);
         VotingService votingService2 = new VotingService();
         votingService2.configureQuestion(question2);
         System.out.println(question2.getType());
@@ -40,7 +40,7 @@ public class SimulationDriver {
 
         
         System.out.println("Question 3:");
-        Question question3 = new Question("Multiple-choice", Arrays.asList("A", "B", "C", "D", "E"), true);
+        Question question3 = new Question("Multiple choices", Arrays.asList("A", "B", "C", "D", "E"), true);
         VotingService votingService3 = new VotingService();
         votingService3.configureQuestion(question3);
         System.out.println(question3.getType());
@@ -49,7 +49,7 @@ public class SimulationDriver {
         
         
         System.out.println("Question 4:");
-        Question question4 = new Question("Single-choice", Arrays.asList("1. True", "2. False"), false);
+        Question question4 = new Question("Single choice", Arrays.asList("1. True", "2. False"), false);
         VotingService votingService4 = new VotingService();
         votingService4.configureQuestion(question2);
         System.out.println(question4.getType());
@@ -58,7 +58,7 @@ public class SimulationDriver {
         
         
         System.out.println("Question 5:");
-        Question question5 = new Question("Multiple-choice", Arrays.asList("A", "B", "C", "D", "E"), true);
+        Question question5 = new Question("Multiple choices", Arrays.asList("A", "B", "C", "D", "E"), true);
         VotingService votingService5 = new VotingService();
         votingService5.configureQuestion(question5);
         System.out.println(question5.getType());
@@ -91,7 +91,10 @@ public class SimulationDriver {
         List<Student> students = new ArrayList<>();
 
         for (int i = 1; i <= numStudents; i++) {
-            students.add(new Student("CS3560_CPP_" + i));
+        	if (i < 10)
+        		students.add(new Student("CS3560100" + Integer.toString(i)));
+        	else
+        		students.add(new Student("CS356010" + Integer.toString(i)));
         }
 
         return students;
